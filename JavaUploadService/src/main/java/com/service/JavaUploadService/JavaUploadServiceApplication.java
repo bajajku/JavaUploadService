9d4fdd5efe5577eb0b864c2b6fb5285773d8fbb5
bajajku <bajajku@sheridancollege.ca>
2025-02-18 16:09:40 -0500

    first commit

---
+++ JavaUploadService/src/main/java/com/service/JavaUploadService/JavaUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.JavaUploadService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUploadServiceApplication.class, args);
+	}
+
+}
---
+++ JavaUploadService/src/test/java/com/service/JavaUploadService/JavaUploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.JavaUploadService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
